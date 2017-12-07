66125b1252399ff3f1b31dc47f0d1a61984c58e7
Irvin <33453856+irvin016@users.noreply.github.com>
2017-12-07 21:27:18 +0530

    Update and rename cardinal.sh to aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/BirdSanctuary/device_xiaomi_kenzo -b oreo device/xiaomi/kenzo
-git clone https://github.com/BirdSanctuary/device_xiaomi_msm8956-common -b oreo-mr1 device/xiaomi/msm8956-common
 git clone https://github.com/BirdSanctuary/kernel_xiaomi_msm8956 -b oreo kernel/xiaomi/msm8956
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi -b lineage-15.0 vendor/xiaomi
 git clone https://github.com/AmolAmrit/android_hardware_cyanogen -b oreo hardware/cyanogen
@@ -11,3 +10,10 @@ git clone https://github.com/Cardinal-AOSP/hardware_qcom_media -b oreo-mr1-caf-8
 git clone https://github.com/Cardinal-AOSP/hardware_qcom_display -b oreo-mr1-caf-8952 hardware/qcom/display-caf/msm8952
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
+cd device/xiaomi/kenzo
+rm -rf aoscp.mk
+rm -r aoscp.dependencies
+gdrive download 1aLVHyJRID4cDXApqX3dXUrfvVi1LczX3
+ls
+cd
+cd pissel
