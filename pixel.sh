07891a208b8d5ac1c4754c1aedef270bf5f1e44a
Irvin <33453856+irvin016@users.noreply.github.com>
2017-12-12 18:27:56 +0000

    update git links
    updated repo links according to my forked repos

--- pixel.sh
+++ pixel.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/AashiqJ/device_xiaomi_kenzo -b o-mr1 device/xiaomi/kenzo
-git clone https://github.com/AashiqJ/device_xiaomi_msm8956-common -b o-mr1 device/xiaomi/msm8956-common
-git clone https://github.com/BirdSanctuary/kernel_xiaomi_msm8956 -b oreo kernel/xiaomi/msm8956
-git clone https://github.com/TheScarastic/proprietary_vendor_xiaomi -b lineage-15.0 vendor/xiaomi
+git clone https://github.com/irvin016/device_xiaomi_kenzo -b oreo-mr1 device/xiaomi/kenzo
+git clone https://github.com/irvin016/device_xiaomi_msm8956-common -b oreo-mr1 device/xiaomi/msm8956-common
+git clone https://github.com/irvin016/kernel_xiaomi_msm8956 -b oreo-mr1 kernel/xiaomi/msm8956
+git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b oreo-mr1 vendor/xiaomi
 git clone https://github.com/AmolAmrit/android_hardware_cyanogen -b oreo hardware/cyanogen
 rm -rf hardware/qcom/audio-caf/msm8952
 rm -rf hardware/qcom/display-caf/msm8952
