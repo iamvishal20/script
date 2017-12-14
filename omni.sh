439a1860bc3a9866b06be71d2e8a9e9c1f1b9a43
Irvin <33453856+irvin016@users.noreply.github.com>
2017-12-14 08:12:23 +0000

    Update and rename aosip.sh to omni.sh

---
+++ omni.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/omnirom/android_device_xiaomi_kenzo -b android-8.0 device/xiaomi/kenzo
+git clone https://github.com/omnirom/android_device_xiaomi_msm8956-common -b android-8.0 device/xiaomi/msm8956-common
+git clone https://github.com/omnirom/android_kernel_xiaomi_msm8956 -b kernel/xiaomi/msm8956
+git clone https://github.com/omnirom/android_hardware_qcom_display-caf-msm8952 -b android-8.0 hardware/qcom/display-caf-msm8952
+git clone https://github.com/omnirom/android_hardware_qcom_audio-caf-msm8952 -b android-8.0 hardware/qcom/audio-caf-msm8952
+git clone https://github.com/omnirom/android_hardware_qcom_media-caf-msm8952 -b android-8.0 hardware/qcom/media-caf-msm8952
+git clone https://github.com/nitrogen-project/android_vendor_xiaomi_kenzo -b o2 vendor/xiaomi/kenzo
+git clone https://github.com/omnirom/android_external_libnfnetlink -b android-8.0 external/libnfnetlink
+git clone https://github.com/omnirom/android_external_libnetfilter_conntrack -b android-8.0 external/libnetfilter/conntrack
+git clone https://github.com/omnirom/android_device_qcom_caf-sepolicy -b android-8.0 device/qcom/caf-sepolicy
+git clone https://github.com/omnirom/vendor_qcom_opensource_dataservices -b android-8.0 vendor/qcom/opensource/dataservices
+git clone https://github.com/omnirom/android_external_ant-wireless_antradio-library -b android-8.0 external/ant-wireless/antradio-library
