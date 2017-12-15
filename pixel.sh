150dfa09e87725890c8a5bb44b160c32e95f7573
root <root@irvin6.c.irvin16-19d39.internal>
2017-12-15 13:58:25 +0000

    Revert "Update pixel.sh"
    This reverts commit 404fab251bbdd096355640a21efaff07eb6c2922.

--- pixel.sh
+++ pixel.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/BirdSanctuary/device_xiaomi_kenzo -b oreo-mr1 device/xiaomi/kenzo
-git clone https://github.com/BirdSanctuary/device_xiaomi_msm8956-common -b oreo-mr1 device/xiaomi/msm8956-common
-git clone https://github.com/BirdSanctuary/kernel_xiaomi_msm8956 -b oreo-mr1 kernel/xiaomi/msm8956
+git clone https://github.com/irvin016/device_xiaomi_kenzo -b oreo-mr1 device/xiaomi/kenzo
+git clone https://github.com/irvin016/device_xiaomi_msm8956-common -b oreo-mr1 device/xiaomi/msm8956-common
+git clone https://github.com/irvin016/kernel_xiaomi_msm8956 -b oreo-mr1 kernel/xiaomi/msm8956
 git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b oreo-mr1 vendor/xiaomi
 git clone https://github.com/AmolAmrit/android_hardware_cyanogen -b oreo hardware/cyanogen
 rm -rf hardware/qcom/audio-caf/msm8952
@@ -11,19 +11,6 @@ git clone https://github.com/Cardinal-AOSP/hardware_qcom_media -b oreo-mr1-caf-8
 git clone https://github.com/Cardinal-AOSP/hardware_qcom_display -b oreo-mr1-caf-8952 hardware/qcom/display-caf/msm8952
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
-cd device/xiaomi/kenzo
-rm -r cardinal.mk
-rm -r cardinal.dependencies
-rm -r vendorsetup.sh
-git clone https://gist.github.com/irvin016/f05d3b7a069bb175f97ee25e8c8dd92b
-cd ..
-cd msm8956-common
-rm -r cardinal.dependencies
-cd ..
-cd ..
-cd ..
-. build/envsetup.sh
-lunch aosp_kenzo-userdebug
 
 
 
