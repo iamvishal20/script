b542b383c5ce4d3cb07b31a3ffb53cc4705d38f5
iamvishal20 <32848958+iamvishal20@users.noreply.github.com>
2017-12-27 22:15:17 +0530

    kernel update

--- aosp.sh
+++ aosp.sh
@@ -1,5 +1,5 @@
 
-git clone https://github.com/BirdSanctuary/kernel_xiaomi_msm8956 -b oreo kernel/xiaomi/msm8956
+git clone https://github.com/BirdSanctuary/kernel_xiaomi_msm8956 -b oreo-mr1-caf kernel/xiaomi/msm8956
 git clone https://github.com/BirdSanctuary/proprietary_vendor_xiaomi -b oreo-mr1 vendor/xiaomi
 git clone https://github.com/iamvishal20/android_device_xiaomi_kenzo -b o2 device/xiaomi/kenzo
 rm -rf hardware/qcom/audio-caf/msm8952
